846a5bce82891cfb6f373d0774b0907f53b2eeae
George Paterson <paterson.george@gmail.com>
2026-04-26 22:57:31 +0100

    Adding Jest

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest/presets/default-esm",
+  testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts"],
+  testMatch: ["**/*.test.ts"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  collectCoverageFrom: ["src/**/*.ts", "!src/**/*.d.ts"],
+};
+
+export default config;
